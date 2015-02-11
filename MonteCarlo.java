d5dc316ca4631732426bc61b4dd6431bf07c1fab
Tranp3 <phu.tran@mrjaffesclass.com>
2015-02-11 02:27:11 -0800

    final commit
    ready to turn in

--- MonteCarlo.java
+++ MonteCarlo.java
@@ -5,29 +5,66 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+import java.util.*;
+import java.lang.Math.*;
+import java.util.Random.*;
 public class MonteCarlo
 {
     // instance variables - replace the example below with your own
-    private int x;
-
+    private double x,y,l,l1;
+    private Random rndm = new Random();
+    public double height;
+    public double width;
+    public double radius;
     /**
      * Constructor for objects of class MonteCarlo
+     * @param height, width, and radius
      */
-    public MonteCarlo()
+    public MonteCarlo(double h,double k,double r)
     {
-        // initialise instance variables
-        x = 0;
+        height = h;
+        width = k;
+        radius = Math.abs(r);   
+        l = h-r;
+        l1 = k-r;
     }
 
     /**
-     * An example of a method - replace this comment with your own
+     * Random rain drop x coordinate;
      * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * @return     return a random x
      */
-    public int sampleMethod(int y)
+    public double nextRainDrop_x()
+    {        
+        x = l + ((2 * radius) * rndm.nextDouble());
+        System.out.println(x);
+        return x;
+    }
+    /**
+     * Random rain drop y coordinate;
+     * 
+     * @return return a random y
+     */
+    public double nextRainDrop_y()
     {
-        // put your code here
-        return x + y;
+        y = l1 + (2 * radius) *rndm.nextDouble();
+        System.out.println(y);
+        return y;
+    }
+    /**
+     * Check if rain drop is inside the circle
+     * 
+     * @return a true or flase
+     */
+    public boolean insideCircle(double x, double y)
+    {
+        if((Math.pow((x - height), 2))+ (Math.pow((y - width), 2)) <= Math.pow(radius,2))
+        {
+         return true;  
+        }
+        else
+        {
+         return false;
+        }
     }
 }
